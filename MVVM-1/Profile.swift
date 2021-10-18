fcf93455258fecfa5abe34bc6bfd16b24e1095ac
Sergey Lobanov <sergey.lobanov@orioninc.com>
2021-10-18 11:54:17 +0300

    Create easy project to test MVVM. Now it has MVC architecture.

---
+++ MVVM-1/Profile.swift
@@ -0,0 +1,15 @@
+//
+//  Profile.swift
+//  MVVM-1
+//
+//  Created by Sergey Lobanov on 18.10.2021.
+//
+
+import Foundation
+
+
+struct Profile {
+    var name: String
+    var secondName: String
+    var age: Int
+}
--- MVVM-1/ViewController.swift
+++ MVVM-1/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak private var nameLabel: UILabel!
+    @IBOutlet weak private var secondNameLabel: UILabel!
+    @IBOutlet weak private var ageLabel: UILabel!
+
+    var profile: Profile? {
+        didSet {
+            guard let profile = profile else { return }
+            self.nameLabel.text = profile.name
+            self.secondNameLabel.text = profile.secondName
+            self.ageLabel.text = "\(profile.age)"
+        }
     }
 
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        profile = Profile(name: "Jonh", secondName: "Smith", age: 33)
+    }
 }
 
